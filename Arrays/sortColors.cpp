374cdd039dbdbdb43dbb87d399034063ce2a4356
Rishabh <rajrishabh643@gmail.com>
2026-07-11 22:52:19 +0530

    Question no 75

---
+++ Arrays/sortColors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int k = i;
+        int j = nums.size() - 1;
+
+        while(k <= j){
+            if(nums[k] == 0){
+                swap(nums[k], nums[i]);
+                i++;
+                k++;
+            }
+            else if(nums[k] == 2){
+                swap(nums[k], nums[j]);
+                j--;
+            }
+            else{
+                k++;
+            }
+            
+       }
+    }
+};
