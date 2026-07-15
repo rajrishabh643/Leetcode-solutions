cf618cd826cf9b0b67460cb1b7e8dd0699321861
Rishabh <rajrishabh643@gmail.com>
2026-07-15 20:17:15 +0530

    Question no 724

---
+++ Binary Search/FindPivotIndex.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            totalSum += nums[i];
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(rightSum == leftSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
