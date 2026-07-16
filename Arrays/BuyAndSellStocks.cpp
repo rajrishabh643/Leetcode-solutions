12f9ec8d8f0a98fc9f550aca0ccdd1c11233029c
Rishabh <rajrishabh643@gmail.com>
2026-07-16 18:13:47 +0530

    Question no 121

---
+++ Arrays/BuyAndSellStocks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+            int profit =prices[i] - buy;
+
+            if(profit > maxProfit){
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+};
