a5ffacee579443bb7144cae9de8b4c5076ae5b70
Rishabh <rajrishabh643@gmail.com>
2026-09-12 02:15:05 +0530

    Question no 1248

---
+++ Arrays/CountNumberofNiceSubarrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int func(vector<int>& nums, int k){
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int count = 0;
+        
+        while(r < nums.size()){
+            sum += nums[r];
+
+            while(sum > k){
+                sum -= nums[l];
+                l++;
+            }
+            count += r - l + 1;
+            r++;
+        }
+        return count;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] % 2 == 0){
+                nums[i] = 0;
+            }
+            else{
+                nums[i] = 1;
+            }
+        }
+
+        return func(nums, k) - func(nums, k -1);
+    }
+};
