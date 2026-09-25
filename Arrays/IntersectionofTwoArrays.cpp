ee40985b2dce2f6e489c694a6ce403859901f6a5
Rishabh <rajrishabh643@gmail.com>
2026-09-25 10:36:21 +0530

    Question no 349

---
+++ Arrays/IntersectionofTwoArrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st;
+        vector<int> ans;
+
+        for(int i: nums1){
+            st.insert(i);
+        }
+
+        for(int i = 0; i < nums2.size(); i++){
+            if(st.find(nums2[i]) != st.end()){
+                ans.push_back(nums2[i]);
+                st.erase(nums2[i]);
+            }
+        }
+
+        return ans;
+    }
+};
