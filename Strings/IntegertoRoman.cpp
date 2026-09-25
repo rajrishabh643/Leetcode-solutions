b14611cc53f0390a60af68b4a9c4ca7f79756285
Rishabh <rajrishabh643@gmail.com>
2026-09-26 02:41:44 +0530

    Question no 12

---
+++ Strings/IntegertoRoman.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int,string>> v = {
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
+        };
+        string ans;
+
+        for(auto i: v){
+            while(num >= i.first){
+                num -= i.first;
+                ans += i.second;
+            }
+        }
+
+        return ans;
+    }
+};
