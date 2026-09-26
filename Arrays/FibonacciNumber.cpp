3ed42d870986311004249a05570a8958c802c346
Rishabh <rajrishabh643@gmail.com>
2026-09-26 08:02:31 +0530

    Question no 509

---
+++ Arrays/FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return a;
+    }
+};
